014b57f3a9e70d3b6f15dfd73647c49d013c27c0
Heinz-Wilhelm Schäbe <heinz-wilhelm@siductionPC>
2015-12-29 11:45:44 +0100

    Programmcode kommentiert

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+# What we are doing here?
+We develop an balloon for the Stratosphere. We are a small group of students from a bording school in southern Germany, near Ulm. In 2014 we had the idea to develop and launch an balloon into the Stratosphere. Our first balloon launched on 21 March 2015 during the partial solar eclipse in Europe. At almost 22 km, our balloon, filled with helium, burst. The capsule landed save, hanging on a parachute.
+
+
+Now we are developing our second balloon. We target this launch in Spring  2016.
 # VERSION2
 Die Daten für V2 des Ballonprojekts 'UgoesSky'.
--- scripte/sstv.sh
+++ scripte/sstv.sh
@@ -1,42 +1,45 @@
 #!/bin/bash
-NUM=0
-timea=0
-timeb=0
-timec=0
-ORT=/home/pi/picam
-PIC=0
-SPIC=0
-echo "25" > /sys/class/gpio/unexport
-echo "25" > /sys/class/gpio/export
-echo "out" > /sys/class/gpio/gpio25/direction
-echo "0" > /sys/class/gpio/gpio25/value
-while true
+#Variablen # IDEA: Programmierung in C
+			#NUM=0 	#Gibts so nicht
+timea=0 #Variable für Warteschleife zw. Sendungen
+timeb=0 #Variable für Warteschleife zw. Sendungen
+timec=0 #Variable für Warteschleife zw. Sendungen
+ORT=/home/pi/picam #Verzeichnis zur Speicherung von Bildern
+PIC=0 # Zähler für verarbeitete hochaflösende Bilder
+SPIC=0 # Zähler für SSTV Bilder
+
+echo "25" > /sys/class/gpio/unexport # Emulierung PTT-Funktion an Funkgerät
+echo "25" > /sys/class/gpio/export  # Emulierung PTT-Funktion an Funkgerät
+echo "out" > /sys/class/gpio/gpio25/direction # Emulierung PTT-Funktion an Funkgerät
+echo "0" > /sys/class/gpio/gpio25/value # Emulierung PTT-Funktion an Funkgerät
+
+while true #Hauptprogramm
 do
 #Variablen
-	DATE=$(date +%Y-%m-%dt%H:%M:%S)
-	echo $DATE
+	DATE=$(date +%Y-%m-%dt%H:%M:%S) #Datum
+	#echo $DATE # braucht man nicht. Nur zu Debugzwecke.
 
 	#make picture
-	echo "0" > /home/pi/werte/sstv.txt
+	echo "0" > /home/pi/werte/sstv.txt   # AFSK an Arduino: Das SSTV-Bild wird gemacht
 
 	raspistill -q 100 -o $ORT/$DATE.jpg -l $ORT/latest.jpg --nopreview
 	PIC=$(($PIC + 1))
 	SPIC=$(($SPIC + 1))
 	echo "$DATE:	Foto Nr. $PIC gemacht -> SSTV $SPIC"
 
-#SERIAL=1
-#SERIAL=$(date +%Y-%m-%dt%H:%M:%S)
+#SERIAL=1 # Was macht das?
+#SERIAL=$(date +%Y-%m-%dt%H:%M:%S) # Was macht das?
 
 	# convert picture
-	echo "1" > /home/pi/werte/sstv.txt
+	echo "1" > /home/pi/werte/sstv.txt  # AFSK an Arduino: SSTV-Bild wird zur Sendung vorbereitet.
 	convert $ORT/latest.jpg -resize '320x240' /home/pi/sstv/latest.jpg #Dateiname beachten
 	echo "$DATE:	SSTV $SPIC verkleinert"
-	sed '/./!d' /home/pi/ausgabe.txt -i /home/pi/ausgabe_leer.txt
+	sed '/./!d' /home/pi/ausgabe.txt -i /home/pi/ausgabe_leer.txt # Mitschnitt der Serielen Schnittstelle Rx vom Arduino
 	echo "$DATE:	SSTV $SPIC SERIAL Ausgabe Leerzeilen entfernt"
-	tail -n 4 /home/pi/ausgabe_leer.txt > /home/pi/sstv/serial.txt
+	tail -n 4 /home/pi/ausgabe_leer.txt > /home/pi/sstv/serial.txt # Abschnitt der letzte 4 Zeilen zur SSTV Sendung; braucht man nicht mehr, wenn AFSK tut.
 	echo "$DATE:	SSTV $SPIC SERIAL Ausgabe 4 Zeilen in Datei"
-	cat /home/pi/sstv/serial.txt
-	SERIAL=`cat /home/pi/sstv/serial.txt`
+	# cat /home/pi/sstv/serial.txt
+	SERIAL=`cat /home/pi/sstv/serial.txt` #Befüllung einer Variable mit Tx
 	echo "$DATE:	SSTV $SPIC SERIAL Ausgabe in  Variable"
 	echo "Serial: $SERIAL"
 	convert -density 90 -pointsize 14 -fill red label:"$SERIAL" /home/pi/sstv/serial.png
@@ -45,18 +48,18 @@ do
 	echo "$DATE:	SSTV $SPIC SERIAL Sendung transparent"
 	composite -geometry +10+175 /home/pi/sstv/serial.png sstv/latest.jpg sstv/sstv.jpg
 	echo "$DATE:	SSTV $SPIC SERIAL Ausgabe in Bild einpflegen"
-echo "DC1MAK-11"
+  echo "DC1MAK-11"
 	composite -geometry +230+220 sstv/dc1mak.png sstv/sstv.jpg sstv/sstv.jpg
-	echo "$DATE:	SSTV $SPIC Rufzeichen in Bild einpflegen" TODO:
+	echo "$DATE:	SSTV $SPIC Rufzeichen in Bild einpflegen"  # TODO: Ja was eigendlich?
 	/home/pi/pisstv /home/pi/sstv/sstv.jpg
 	echo "$DATE:	SSTV $SPIC Konvertierung Martin 1"
-	echo "2" > /home/pi/sstv.txt
-	echo "1" > /sys/class/gpio/gpio25/value
-	sleep 0.5
+	echo "2" > /home/pi/sstv.txt # AFSK an Arduino: Sendung steht bevor!
+	echo "1" > /sys/class/gpio/gpio25/value # Emulierung PTT-Funktion an Funkgerät
+	sleep 0.5	#	Funkgerät aktiviert Sendeendstufe
 	echo "$DATE:	SSTV $SPIC SENDUNG BEGINN"
-	aplay /home/pi/sstv/sstv.jpg.wav
+	aplay /home/pi/sstv/sstv.jpg.wav	# SENDUNG VIA FUNK
 	echo "$DATE:	SSTV $SPIC SENDUNG ENDE"
-	echo "0" > /sys/class/gpio/gpio25/value
+	echo "0" > /sys/class/gpio/gpio25/value # Ausschalten PTT-Funktion
 	#save transmitted picture
 	cp /home/pi/sstv/sstv.jpg /home/pi/sstv/save/$DATE.jpg
 # wir warten 2 Min mit dem nächsten Sendung. Solange machen wir hochauflösende Bilder
@@ -64,7 +67,7 @@ echo "DC1MAK-11"
 timea=$(date +%s)
 timeb=$(date +%s)
 timec=$(( timea + 120 ))
-echo "3" > /home/pi/werte/sstv.txt
+echo "3" > /home/pi/werte/sstv.txt   # AFSK an Arduino: Warte mit SSTV; mache schöne Bilder
 while (( $timec > $timeb ))
 do
 	PIC=$(($PIC + 1))
