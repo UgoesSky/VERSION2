b65cb30a40424ba03757cee9842305242e15ad31
jst <git@gappert.de>
2016-06-30 21:26:49 +0200

    fix issue with endless loop when BMP085 not available

--- Software/Umweltdaten/GY88/BMP085/BMP085.ino
+++ Software/Umweltdaten/GY88/BMP085/BMP085.ino
@@ -29,9 +29,9 @@ int md;
 
 // b5 is calculated in bmp085GetTemperature(...), this variable is also used in bmp085GetPressure(...)
 // so ...Temperature(...) must be called before ...Pressure(...).
-long b5; 
+long b5;
 
-void setup(){
+void setup() {
   Serial.begin(9600);
   Wire.begin();
 
@@ -43,7 +43,7 @@ void loop()
   float temperature = bmp085GetTemperature(bmp085ReadUT()); //MUST be called first
   float pressure = bmp085GetPressure(bmp085ReadUP());
   float atm = pressure / 101325; // "standard atmosphere"
-  float altitude = calcAltitude(pressure); //Uncompensated caculation - in Meters 
+  float altitude = calcAltitude(pressure); //Uncompensated caculation - in Meters
 
   Serial.print("Temperature: ");
   Serial.print(temperature, 2); //display 2 decimal places
@@ -84,15 +84,15 @@ void bmp085Calibration()
 }
 
 // Calculate temperature in deg C
-float bmp085GetTemperature(unsigned int ut){
+float bmp085GetTemperature(unsigned int ut) {
   long x1, x2;
 
-  x1 = (((long)ut - (long)ac6)*(long)ac5) >> 15;
-  x2 = ((long)mc << 11)/(x1 + md);
+  x1 = (((long)ut - (long)ac6) * (long)ac5) >> 15;
+  x2 = ((long)mc << 11) / (x1 + md);
   b5 = x1 + x2;
 
-  float temp = ((b5 + 8)>>4);
-  temp = temp /10;
+  float temp = ((b5 + 8) >> 4);
+  temp = temp / 10;
 
   return temp;
 }
@@ -101,33 +101,33 @@ float bmp085GetTemperature(unsigned int ut){
 // calibration values must be known
 // b5 is also required so bmp085GetTemperature(...) must be called first.
 // Value returned will be pressure in units of Pa.
-long bmp085GetPressure(unsigned long up){
+long bmp085GetPressure(unsigned long up) {
   long x1, x2, x3, b3, b6, p;
   unsigned long b4, b7;
 
   b6 = b5 - 4000;
   // Calculate B3
-  x1 = (b2 * (b6 * b6)>>12)>>11;
-  x2 = (ac2 * b6)>>11;
+  x1 = (b2 * (b6 * b6) >> 12) >> 11;
+  x2 = (ac2 * b6) >> 11;
   x3 = x1 + x2;
-  b3 = (((((long)ac1)*4 + x3)<<OSS) + 2)>>2;
+  b3 = (((((long)ac1) * 4 + x3) << OSS) + 2) >> 2;
 
   // Calculate B4
-  x1 = (ac3 * b6)>>13;
-  x2 = (b1 * ((b6 * b6)>>12))>>16;
-  x3 = ((x1 + x2) + 2)>>2;
-  b4 = (ac4 * (unsigned long)(x3 + 32768))>>15;
+  x1 = (ac3 * b6) >> 13;
+  x2 = (b1 * ((b6 * b6) >> 12)) >> 16;
+  x3 = ((x1 + x2) + 2) >> 2;
+  b4 = (ac4 * (unsigned long)(x3 + 32768)) >> 15;
 
-  b7 = ((unsigned long)(up - b3) * (50000>>OSS));
+  b7 = ((unsigned long)(up - b3) * (50000 >> OSS));
   if (b7 < 0x80000000)
-    p = (b7<<1)/b4;
+    p = (b7 << 1) / b4;
   else
-    p = (b7/b4)<<1;
+    p = (b7 / b4) << 1;
 
-  x1 = (p>>8) * (p>>8);
-  x1 = (x1 * 3038)>>16;
-  x2 = (-7357 * p)>>16;
-  p += (x1 + x2 + 3791)>>4;
+  x1 = (p >> 8) * (p >> 8);
+  x1 = (x1 * 3038) >> 16;
+  x2 = (-7357 * p) >> 16;
+  p += (x1 + x2 + 3791) >> 4;
 
   long temp = p;
   return temp;
@@ -143,10 +143,12 @@ char bmp085Read(unsigned char address)
   Wire.endTransmission();
 
   Wire.requestFrom(BMP085_ADDRESS, 1);
-  while(!Wire.available())
-    ;
+  if (Wire.available())
+  {
+    data = Wire.read();
+  }
 
-  return Wire.read();
+  return data;
 }
 
 // Read 2 bytes from the BMP085
@@ -161,16 +163,16 @@ int bmp085ReadInt(unsigned char address)
   Wire.endTransmission();
 
   Wire.requestFrom(BMP085_ADDRESS, 2);
-  while(Wire.available()<2)
-    ;
-  msb = Wire.read();
-  lsb = Wire.read();
-
-  return (int) msb<<8 | lsb;
+  if (Wire.available() >= 2)
+  {
+    msb = Wire.read();
+    lsb = Wire.read();
+  }
+  return (int) msb << 8 | lsb;
 }
 
 // Read the uncompensated temperature value
-unsigned int bmp085ReadUT(){
+unsigned int bmp085ReadUT() {
   unsigned int ut;
 
   // Write 0x2E into Register 0xF4
@@ -189,7 +191,7 @@ unsigned int bmp085ReadUT(){
 }
 
 // Read the uncompensated pressure value
-unsigned long bmp085ReadUP(){
+unsigned long bmp085ReadUP() {
 
   unsigned char msb, lsb, xlsb;
   unsigned long up = 0;
@@ -198,30 +200,30 @@ unsigned long bmp085ReadUP(){
   // Request a pressure reading w/ oversampling setting
   Wire.beginTransmission(BMP085_ADDRESS);
   Wire.write(0xF4);
-  Wire.write(0x34 + (OSS<<6));
+  Wire.write(0x34 + (OSS << 6));
   Wire.endTransmission();
 
   // Wait for conversion, delay time dependent on OSS
-  delay(2 + (3<<OSS));
+  delay(2 + (3 << OSS));
 
   // Read register 0xF6 (MSB), 0xF7 (LSB), and 0xF8 (XLSB)
   msb = bmp085Read(0xF6);
   lsb = bmp085Read(0xF7);
   xlsb = bmp085Read(0xF8);
 
-  up = (((unsigned long) msb << 16) | ((unsigned long) lsb << 8) | (unsigned long) xlsb) >> (8-OSS);
+  up = (((unsigned long) msb << 16) | ((unsigned long) lsb << 8) | (unsigned long) xlsb) >> (8 - OSS);
 
   return up;
 }
 
 void writeRegister(int deviceAddress, byte address, byte val) {
-  Wire.beginTransmission(deviceAddress); // start transmission to device 
+  Wire.beginTransmission(deviceAddress); // start transmission to device
   Wire.write(address);       // send register address
   Wire.write(val);         // send value to write
   Wire.endTransmission();     // end transmission
 }
 
-int readRegister(int deviceAddress, byte address){
+int readRegister(int deviceAddress, byte address) {
 
   int v;
   Wire.beginTransmission(deviceAddress);
@@ -230,21 +232,22 @@ int readRegister(int deviceAddress, byte address){
 
   Wire.requestFrom(deviceAddress, 1); // read a byte
 
-  while(!Wire.available()) {
-    // waiting
+  if (Wire.available())
+  {
+    v = Wire.read();
   }
 
-  v = Wire.read();
+
   return v;
 }
 
-float calcAltitude(float pressure){
+float calcAltitude(float pressure) {
 
-  float A = pressure/101325;
-  float B = 1/5.25588;
-  float C = pow(A,B);
+  float A = pressure / 101325;
+  float B = 1 / 5.25588;
+  float C = pow(A, B);
   C = 1 - C;
-  C = C /0.0000225577;
+  C = C / 0.0000225577;
 
   return C;
 }
