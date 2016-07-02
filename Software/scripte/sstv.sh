83ac709714de01638d44b1cbba5d40f4cc76168a
Heinz-Wilhelm Schäbe <pepsclena@gmx.de>
2016-07-02 20:27:23 +0200

    Update sstv.
    some trouble

--- Software/scripte/sstv.sh
+++ Software/scripte/sstv.sh
@@ -6,7 +6,7 @@ timea=0 #Variable für Warteschleife zw. Sendungen
 timeb=0 #Variable für Warteschleife zw. Sendungen
 timec=0 #Variable für Warteschleife zw. Sendungen
 ORT=/home/pi/picam #Verzeichnis zur Speicherung von Bildern
-PIC=0 # Zähler für verarbeitete hochaflösende Bilder
+PIC=0 # Zähler für verarbeitete hochauflösende Bilder
 SPIC=0 # Zähler für SSTV Bilder
 
 #echo "25" > /sys/class/gpio/unexport # Emulierung PTT-Funktion an Funkgerät
@@ -21,7 +21,7 @@ do
 	#echo $DATE [# braucht man nicht. Nur zu Debugzwecke.]
 
 	#make picture
-	echo "0" > /home/pi/werte/sstv.txt   # AFSK an Arduino: Das SSTV-Bild wird gemacht
+	#echo "0" > /home/pi/werte/sstv.txt   # AFSK an Arduino: Das SSTV-Bild wird gemacht
 
 	raspistill -q 100 -o $ORT/$DATE.jpg -l $ORT/latest.jpg --nopreview
 	PIC=$(($PIC + 1))
@@ -32,7 +32,7 @@ do
 #SERIAL=$(date +%Y-%m-%dt%H:%M:%S) # Was macht das?
 
 	# convert picture
-	echo "1" > /home/pi/werte/sstv.txt  # AFSK an Arduino: SSTV-Bild wird zur Sendung vorbereitet.
+	#echo "1" > /home/pi/werte/sstv.txt  # AFSK an Arduino: SSTV-Bild wird zur Sendung vorbereitet.
 	convert $ORT/latest.jpg -resize '320x240' /home/pi/sstv/latest.jpg #Dateiname beachten
 	echo "$DATE:	SSTV $SPIC verkleinert"
 
@@ -42,20 +42,20 @@ do
   tail -n 1 /home/pi/temp.txt > /home/pi/sstv/send.txt
 	#TODO: Zeilenumbruch nach '%'
 	echo "$DATE:	SSTV $SPIC TEMP Datensatz ausgewählt"
-	TEMP=`cat /home/pi/sstv/send.txt` #Befüllung de Variable mit Daten DHT 22
+	TEMP=`cat /home/pi/sstv/send.txt` #Befüllung der Variable mit Daten DHT 22
 	echo "$DATE:	SSTV $SPIC TEMP Ausgabe in  Variable"
 	echo "Serial: $TEMP"
 	convert -density 90 -pointsize 14 -fill red label:"$TEMP" /home/pi/sstv/serial.png
 	echo "$DATE:	SSTV $SPIC TEMP Ausgabe als Bild"
 	convert /home/pi/sstv/serial.png -transparent white /home/pi/sstv/serial.png
 	echo "$DATE:	SSTV $SPIC TEMP Hintergrund transparent"
-	composite -geometry +10+175 /home/pi/sstv/serial.png sstv/latest.jpg sstv/sstv.jpg
+	composite -geometry +10+175 /home/pi/sstv/serial.png sstv/latest.jpg /home/pi/sstv/sstv.jpg
 	echo "$DATE:	SSTV $SPIC TEMP Datensatz in Bild einpflegen"
   echo "$DATE:	SSTV $SPIC SSTV Rufzeichen DC1MAK-11 in Bild einpflegen"
-	composite -geometry +230+220 sstv/dc1mak.png sstv/sstv.jpg sstv/sstv.jpg
+	composite -geometry +210+220 sstv/dc1mak.png sstv/sstv.jpg /home/pi/sstv/sstv.jpg
   echo "$DATE:	SSTV $SPIC Konvertierung Martin 1"
 	/home/pi/pisstv /home/pi/sstv/sstv.jpg
-	echo "2" > /home/pi/sstv.txt # AFSK an Arduino: Sendung steht bevor!
+	#echo "2" > /home/pi/sstv.txt # AFSK an Arduino: Sendung steht bevor!
 	#echo "1" > /sys/class/gpio/gpio25/value # Emulierung PTT-Funktion an Funkgerät #XXX: Ist das notwendig?
 	#sleep 0.5	#	Funkgerät aktiviert Sendeendstufe
 	echo "$DATE:	SSTV $SPIC SENDUNG BEGINN"
@@ -69,11 +69,12 @@ do
 timea=$(date +%s)
 timeb=$(date +%s)
 timec=$(( timea + 120 ))
-echo "3" > /home/pi/werte/sstv.txt   # AFSK an Arduino: Warte mit SSTV; mache schöne Bilder
+#echo "3" > /home/pi/werte/sstv.txt   # AFSK an Arduino: Warte mit SSTV; mache schöne Bilder
 while (( $timec > $timeb ))
 do
 	PIC=$(($PIC + 1))
-	raspistill -q 100 -o $ORT/$DATE.jpg -l $ORT/latest.jpg --nopreview
+	DATE=$(date +%Y-%m-%dt%H:%M:%S) #Datum
+	raspistill -q 100 -o $ORT/$DATE.jpg #-l $ORT/latest.jpg --nopreview
 	timeb=$(date +%s)
 	echo "$DATE:	Foto Nr. $PIC gemacht"
 done
